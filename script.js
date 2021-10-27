291c80f2c2096fdc864753fc7fd7ba0a93d287c6
esthersmoothie <91024352+esthersmoothie@users.noreply.github.com>
2021-10-27 11:34:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let nome = prompt("Digite seu nome :D")
+
+let mensagem = `Olá ${nome} seja bem vinde, esse foi o meu primeiro site :D`
+
+let ano = 2021
+let nascimento = prompt("Digite seu ano de nascimento ;D")
+let idade = ano - nascimento
+
+let mensagem2 = `Pelo o que você me disse acho que vc tem uns... ${idade} anos ;)`
+
+alert( mensagem )
+alert( mensagem2 )
