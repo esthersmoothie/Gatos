c71cec31b4a01bb68f3f39f65d8f2b7bef416e03
esthersmoothie <91024352+esthersmoothie@users.noreply.github.com>
2021-11-01 10:15:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let nome = prompt("Digite seu nome :D")
 
-let mensagem = `Olá ${nome} seja bem vinde, esse foi o meu primeiro site :D`
+let mensagem1 = `Olá ${nome} seja bem vinde, esse foi o meu primeiro site :D`
 
 let ano = 2021
 let nascimento = prompt("Digite seu ano de nascimento ;D")
 let idade = ano - nascimento
 
 let mensagem2 = `Pelo o que você me disse acho que vc tem uns... ${idade} anos ;)`
+let mensagem3 = `Pelo o que você me disse acho que vc tem uns... ${idade} anos ;), você já é um adolecente`
+let mensagem4 = `Pelo o que você me disse acho que vc tem uns... ${idade} anos ;), você é uma criança`
 
-alert( mensagem )
-alert( mensagem2 )
+
+alert( mensagem1 )
+
+if ( idade >= 12 ) {alert ( mensagem3 ) } 
+else if ( idade < 12 ) {alert ( mensagem4) } 
+else {alert ( mensagem2 ) }
